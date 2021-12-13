87404d95d5a25d71757cd17846fa83cf0e1a1392
Arthur Magno <arthurmagno@arthurmagno.com>
2021-12-13 07:50:16 -0300

    Main file

---
+++ carrossel.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Carrossel
+Author: Arthur Magno
+Version: 0.0.1
+Author URI: www.arthurmagno.com
+License: GPLv2 or later
+Text Domain: carrossel
+Plugin URI: www.arthurmagno.com
+Description: Carrossel.
+*/
+
+?>
